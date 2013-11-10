fec0db61f2511d605b5d7aa73630f74f47f6856d
Stoneharry <stoneharryjames@gmail.com>
2013-11-10 20:29:37 +0000

    Fill with stuff.

---
+++ Blizz Downloader/TinyTracker/Program.cs
@@ -0,0 +1,65 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Net;
+
+namespace TinyTracker
+{
+	class Program
+	{
+		static byte[] GetCompactPeerAddress(string ip, string port)
+		{
+			byte[] buffer = new byte[6];
+			IPAddress add = IPAddress.Parse(ip);
+			add.GetAddressBytes().CopyTo(buffer, 0);
+			BitConverter.GetBytes(ushort.Parse(port)).CopyTo(buffer, 4);
+
+			return buffer;
+		}
+
+		static void Main(string[] args)
+		{
+			HttpListener listener = new HttpListener();
+			listener.Prefixes.Clear();
+            listener.Prefixes.Add("http://*:3724/announce/");
+
+			listener.Start();
+
+			while (true)
+			{
+				HttpListenerContext context = listener.GetContext();
+
+				HttpListenerRequest req = context.Request;
+
+				string ip = req.QueryString["ip"];
+				string port = req.QueryString["port"];
+				string public_ip = req.RemoteEndPoint.Address.ToString();
+				string info_hash = req.QueryString["info_hash"];
+
+				//string directDownloadURL = "http://blizzard.vo.llnwd.net/o16/content/ptr/WoW-4.0.0.12319-to-4.0.0.12479-enUS-patch.exe";
+                //string directDownloadURL = "http://eoc.dispersion-wow.com/resources/Wow.exe";
+                string directDownloadURL = "http://eoc.dispersion-wow.com/resources/Repair.exe";
+				string s = "d6:directd3:url" + directDownloadURL.Length + ":" + directDownloadURL +
+					"9:thresholdi10000000ee8:intervali1800e2:ip" + public_ip.Length + ":" + public_ip + "5:peers6:";
+				
+				//	Get response string and add 1 peer (compact address) wich actually is requester himself
+				byte[] buffer = Encoding.UTF8.GetBytes(s);
+				Array.Resize(ref buffer, buffer.Length + 7);
+				GetCompactPeerAddress(ip, port).CopyTo(buffer, buffer.Length - 7);
+				buffer[buffer.Length - 1] = (byte)'e';
+
+				//	Set up response
+				HttpListenerResponse resp = context.Response;
+				resp.ContentType = null;
+				resp.StatusCode = 200;
+				resp.ContentLength64 = buffer.Length;
+
+				//	send response string
+				System.IO.Stream output = resp.OutputStream;
+				output.Write(buffer, 0, buffer.Length);
+			}
+		}
+	}
+}
---
+++ Blizz Downloader/fileSplitter/Program.cs
@@ -0,0 +1,81 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace fileSplitter
+{
+	class Program
+	{
+		static int Main(string[] args)
+		{
+			if (args.Length != 3)
+				Console.WriteLine("usage: fileSplitter <file> <blocksize> <outDir>");
+
+			System.IO.FileStream fs = null;
+			int blockSize = 0;
+			string outDir = null;
+
+			try
+			{
+				fs = new System.IO.FileStream(args[0], System.IO.FileMode.Open);
+
+				if (args[1].StartsWith("0x"))
+					blockSize = Convert.ToInt32(args[1], 16);
+				else
+					blockSize = Convert.ToInt32(args[1], 10);
+
+				if (!System.IO.Directory.Exists(args[2]))
+					outDir = System.IO.Directory.CreateDirectory(args[2]).FullName + '\\';
+				else
+					outDir = new System.IO.DirectoryInfo(args[2]).FullName + "\\";
+			}
+			catch (Exception e)
+			{
+				Console.WriteLine(e.Message);
+				return 1;
+			}
+
+			Console.WriteLine("Splitting {0} to {1} into {2} bytes blocks.", fs.Name, outDir, blockSize);
+
+			long remaining = fs.Length;
+			int packetNbr = 0;
+
+			System.IO.StreamWriter hashFile = new System.IO.StreamWriter(outDir + "hash.txt");
+
+			int qzdqzdqd = 0;
+
+			while (remaining > 0)
+			{
+				//	Write packet file
+				System.IO.FileStream packetFile = new System.IO.FileStream(outDir + packetNbr.ToString(), System.IO.FileMode.Create);
+				
+				byte[] buffer = new byte[blockSize];
+				int BytesRead = fs.Read(buffer, 0, blockSize);
+
+				Console.WriteLine("Writting {0} to {1}.", BytesRead, packetFile.Name);
+				packetFile.Write(buffer, 0, BytesRead);
+				packetFile.Flush();
+
+				//	Calculate sha1 hash
+				System.Security.Cryptography.SHA1 sha1CSP = new System.Security.Cryptography.SHA1CryptoServiceProvider();
+				byte[] hash = sha1CSP.ComputeHash(buffer, 0, BytesRead);
+
+				string sHash = BitConverter.ToString(hash);
+				sHash = sHash.Replace("-", "");
+				qzdqzdqd += sHash.Length;
+
+				//	write hash
+				hashFile.Write(sHash);
+
+				remaining -= BytesRead;
+				packetNbr++;
+			}
+
+			hashFile.Flush();
+
+			return 0;
+		}
+	}
+}
